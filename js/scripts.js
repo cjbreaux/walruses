29fb2520f80f553a4e3b7b8ed90e54d6911325ea
Chris Breaux and Tracy Jo Reith <tracy@>
2019-01-15 09:31:06 -0800

    Add toggle effects to individual walrus images

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
     $("#fade-toggle").fadeToggle();
     $("#slide-toggle").slideToggle();
   });
+
+  $("#toggle").click(function() {
+    $("#toggle").toggle();
+  });
+
+  $("#fade-toggle").click(function() {
+    $("#fade-toggle").fadeToggle();
+  });
+
+  $("#slide-toggle").click(function() {
+    $("#slide-toggle").slideToggle();
+  });
 });
