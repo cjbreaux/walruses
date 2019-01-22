41af8d21e493dc2f4ec73b93e4f811db66de7539
Student <example@example.com>
2019-01-22 11:59:50 -0800

    change scripts to add forEach loop to display elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,39 @@
 $(document).ready(function(){
-  $("h1").click(function() {
-    $("#toggle").toggle();
-    $("#fade-toggle").fadeToggle();
-    $("#slide-toggle").slideToggle();
-  });
+  var elements = ["h1","p","img"];
+  elements.forEach(function(element){
+    $(element).click(function(){
+      alert("you clicked an " + element +" element")
+    })
+  })
 
-  $("#toggle").click(function() {
-    $("#toggle").toggle();
-  });
-
-  $("#fade-toggle").click(function() {
-    $("#fade-toggle").fadeToggle();
-  });
-
-  $("#slide-toggle").click(function() {
-    $("#slide-toggle").slideToggle();
-  });
-
-  $(".instant p").click(function() {
-    $("#toggle").toggle();
-  });
-
-  $(".fading p").click(function() {
-    $("#fade-toggle").fadeToggle();
-  });
-
-  $(".slide p").click(function() {
-    $("#slide-toggle").slideToggle();
-  });
+  // $("h1").click(function() {
+  //   $("#toggle").toggle();
+  //   $("#fade-toggle").fadeToggle();
+  //   $("#slide-toggle").slideToggle();
+  // });
+  //
+  // $("#toggle").click(function() {
+  //   $("#toggle").toggle();
+  // });
+  //
+  // $("#fade-toggle").click(function() {
+  //   $("#fade-toggle").fadeToggle();
+  // });
+  //
+  // $("#slide-toggle").click(function() {
+  //   $("#slide-toggle").slideToggle();
+  // });
+  //
+  // $(".instant p").click(function() {
+  //   $("#toggle").toggle();
+  // });
+  //
+  // $(".fading p").click(function() {
+  //   $("#fade-toggle").fadeToggle();
+  // });
+  //
+  // $(".slide p").click(function() {
+  //   $("#slide-toggle").slideToggle();
+  // });
 
 });
